76149213ee68283e3a71f00f33826bc42a83589d
Vinícius Gabriel Santos Bezerra <168607523+ViniGS2@users.noreply.github.com>
2025-03-01 23:48:56 -0300

    Delete enumeracoes directory

--- enumeracoes/src/enums/Status.java
+++
@@ -1,9 +0,0 @@
-package enums;
-
-public enum Status {
-    PAGAMENTO_PENDENTE,
-    PROCESSANDO,
-    ENVIADO,
-    ENTREGUE;
-    //Serve para a organização do sistema ((nesse caso de um processamento de um pedido
-}
